f6e4c07b4841691f44882aaa53eb2889f306e4ee
Brett Petch <brettpetch@icloud.com>
2021-01-16 16:57:13 -0500

    back end

--- server.js
+++ server.js
@@ -12,49 +12,20 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.post('/api/v1/sentiment', (req, res) => {
-    // take in the sentence as the input
-    const sentence = req.body;
-    // log the request
-    console.log(sentence)
-    sentence.push(sentence)
-    
-    res.json(intensity)
-});
-
-
-app.post('/api/v1/week', (req, res) => {
-    const sentence = req.body;
-
-    console.log(sentence)
-    sentence.push(sentence)
-
-    var intensity = vader.SentimentIntensityAnalyzer.polarity_scores(sentence);
-    res.send(intensity)
-});
-
-app.post('/api/v1/recommendation', (req, res) => {
-    const sentence = req.body;
-
-    console.log(sentence)
-    sentence.push(sentence)
-
-    res.send(intensity)
-});
-
-app.get('/api/v1/:user', (req, res) => {
-    // Reading isbn from the URL
-    const isbn = req.params.user;
-
-    // Searching books for the isbn
-    for (let user of users) {
-        if (users.user === user) {
-            res.json(user);
-            return;
-        }
-    }
-    // Sending 404 when not found something is a good practice
-    res.status(404).send('Book not found');
+app.post('/api/v1/response', (req, res) => {
+    // Pull in data
+    obj = JSON.parse(req.body);
+    // setup score dict
+    scores = {c_score: 0, h_score: 0, i_score=0, m_score=0, e_score=0};
+    scores.c_score = vader.SentimentIntensityAnalyzer.polarity_scores(obj.c_sentence);
+    scores.h_score = vader.SentimentIntensityAnalyzer.polarity_scores(obj.h_sentence);
+    scores.i_score = vader.SentimentIntensityAnalyzer.polarity_scores(obj.i_sentence);
+    scores.m_score = vader.SentimentIntensityAnalyzer.polarity_scores(obj.m_sentence);
+    scores.e_score = vader.SentimentIntensityAnalyzer.polarity_scores(obj.e_sentence);
+    // log the scores
+    console.log(scores);
+    // 
+    res.json(scores);
 });
 
 app.listen(port, () => console.log(`Hello world app listening on port ${port}!`));
