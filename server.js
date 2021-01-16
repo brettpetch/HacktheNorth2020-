19955e13c11cfce6fbbb67c5335b730c25d1b425
Brett Petch <brettpetch@icloud.com>
2021-01-16 12:46:42 -0500

    Backend Setup

---
+++ server.js
@@ -0,0 +1,8 @@
+const vader = require('vader-sentiment');
+const input = 'VADER is very smart, handsome, and funny';
+const intensity = vader.SentimentIntensityAnalyzer.polarity_scores(input);
+console.log(intensity);
+
+http.createServer(function (req, res) {
+    runInNewContext.writeHead(200 {'Content-Type': ''})
+})
