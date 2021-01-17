765e832d76332870e40367a167eaa322c062a115
Abdalla Abdelhady <bla2030@live.com>
2021-01-17 09:14:39 -0500

    updated questions and package.json

--- src/Question.js
+++ src/Question.js
@@ -39,23 +39,23 @@ export class Question extends React.Component {
             <div className="Question">
                 <form id="sentimetre" onSubmit={this.handleSubmit.bind(this)} method="POST">
                     <div className="form-group">
-                        <label htmlFor="c_sentence">Do you feel supported by the people around you?</label>
+                        <label htmlFor="c_sentence">How supported do you feel by the people around you?</label>
                         <input type="text" className="form-control" id="c_sentence" value={this.state.c_sentence} onChange={this.onCChange.bind(this)} />
                     </div>
                     <div className="form-group">
-                        <label htmlFor="h_sentence">Are you motivated to change your life?</label>
+                        <label htmlFor="h_sentence">Are you motivated to change your life? How?</label>
                         <input type="text" className="form-control" id="h_sentence" value={this.state.h_sentence} onChange={this.onHChange.bind(this)} />
                     </div>
                     <div className="form-group">
-                        <label htmlFor="i_sentence">Do you feel judged for being yourself?</label>
+                        <label htmlFor="i_sentence">How genuine do you feel in your daily life?</label>
                         <input type="text" className="form-control" id="i_sentence" value={this.state.i_sentence} onChange={this.onIChange.bind(this)} />
                     </div>
                     <div className="form-group">
-                        <label htmlFor="m_sentence">Do you have goals that you strive towards?</label>
+                        <label htmlFor="m_sentence">What are your goals and dreams, if any?</label>
                         <input type="text" className="form-control" id="m_sentence" value={this.state.m_sentence} onChange={this.onMChange.bind(this)} />
                     </div>
                     <div className="form-group">
-                        <label htmlFor="e_sentence">Do you feel in control of your life?</label>
+                        <label htmlFor="e_sentence">How in control do you feel in your life?</label>
                         <input type="text" className="form-control" id="e_sentence" value={this.state.e_sentence} onChange={this.onEChange.bind(this)} />
                     </div>
                     <br/>
