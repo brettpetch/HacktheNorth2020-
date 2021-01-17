a17c6531e30b18a06f82a1614d37d351f1671c01
Brett Petch <brettpetch@icloud.com>
2021-01-16 20:23:03 -0500

    upgrade all the things

--- server/server.js
+++ server/server.js
@@ -6,28 +6,27 @@ const cors = require('cors');
 const app = express();
 const port = 3000;
 
-app.use(cors());
-const average = arr => arr.reduce((sume, el) => sume + el, 0) / arr.length;    
-
-// Configuring body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
+app.use(cors());
+let average = (array) => array.reduce((a, b) => a + b) / array.length;
+// Configuring body parser middleware
 app.post('/api/v1/response', (req, res) => {
     // Pull in data
-    obj = JSON.parse(req.body);
-    // setup score dict
+    let stuff = {}
+    const dat = req.body
+    console.log(dat);
     scores = {}
-    scores.c_score = vader.SentimentIntensityAnalyzer.polarity_scores(obj.c_sentence);
-    scores.h_score = vader.SentimentIntensityAnalyzer.polarity_scores(obj.h_sentence);
-    scores.i_score = vader.SentimentIntensityAnalyzer.polarity_scores(obj.i_sentence);
-    scores.m_score = vader.SentimentIntensityAnalyzer.polarity_scores(obj.m_sentence);
-    scores.e_score = vader.SentimentIntensityAnalyzer.polarity_scores(obj.e_sentence);
-    scores.avg = average(Object.values(scores));
-    // log the scores
+    scores.c_score = vader.SentimentIntensityAnalyzer.polarity_scores(dat.c_sentence);
+    scores.h_score = vader.SentimentIntensityAnalyzer.polarity_scores(dat.h_sentence);
+    scores.i_score = vader.SentimentIntensityAnalyzer.polarity_scores(dat.i_sentence);
+    scores.m_score = vader.SentimentIntensityAnalyzer.polarity_scores(dat.m_sentence);
+    scores.e_score = vader.SentimentIntensityAnalyzer.polarity_scores(dat.e_sentence);
+    let compound = Object.values(scores).map(ele => ele.compound)
+    scores.compound = average(compound)    
     console.log(scores);
-    // 
-    res.json(scores);
+    console.log(compound);
+    res.json(scores)
 });
 
 app.listen(port, () => console.log(`Hello world app listening on port ${port}!`));
